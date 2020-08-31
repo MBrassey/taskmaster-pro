d91a2f7c97a8f0532a894cc65c10bcf163861fd2
luc1dLife <luc1d_life@protonmail.com>
2020-08-31 13:29:09 -0600

    Finished Bootstrap

--- assets/js/script.js
+++ assets/js/script.js
@@ -102,54 +102,40 @@ $("#task-form-modal .btn-primary").click(function () {
 });
 
 // due date was clicked
-$(".list-group").on("click", "span", function() {
-  // get current text
-  var date = $(this)
-    .text()
-    .trim();
+$(".list-group").on("click", "span", function () {
+    // get current text
+    var date = $(this).text().trim();
 
-  // create new input element
-  var dateInput = $("<input>")
-    .attr("type", "text")
-    .addClass("form-control")
-    .val(date);
+    // create new input element
+    var dateInput = $("<input>").attr("type", "text").addClass("form-control").val(date);
 
-  // swap out elements
-  $(this).replaceWith(dateInput);
+    // swap out elements
+    $(this).replaceWith(dateInput);
 
-  // automatically focus on new element
-  dateInput.trigger("focus");
+    // automatically focus on new element
+    dateInput.trigger("focus");
 });
 
 // value of due date was changed
-$(".list-group").on("blur", "input[type='text']", function() {
-  // get current text
-  var date = $(this)
-    .val()
-    .trim();
+$(".list-group").on("blur", "input[type='text']", function () {
+    // get current text
+    var date = $(this).val().trim();
 
-  // get the parent ul's id attribute
-  var status = $(this)
-    .closest(".list-group")
-    .attr("id")
-    .replace("list-", "");
+    // get the parent ul's id attribute
+    var status = $(this).closest(".list-group").attr("id").replace("list-", "");
 
-  // get the task's position in the list of other li elements
-  var index = $(this)
-    .closest(".list-group-item")
-    .index();
+    // get the task's position in the list of other li elements
+    var index = $(this).closest(".list-group-item").index();
 
-  // update task in array and re-save to localstorage
-  tasks[status][index].date = date;
-  saveTasks();
+    // update task in array and re-save to localstorage
+    tasks[status][index].date = date;
+    saveTasks();
 
-  // recreate span element with bootstrap classes
-  var taskSpan = $("<span>")
-    .addClass("badge badge-primary badge-pill")
-    .text(date);
+    // recreate span element with bootstrap classes
+    var taskSpan = $("<span>").addClass("badge badge-primary badge-pill").text(date);
 
-  // replace input with span element
-  $(this).replaceWith(taskSpan);
+    // replace input with span element
+    $(this).replaceWith(taskSpan);
 });
 
 // remove all tasks
